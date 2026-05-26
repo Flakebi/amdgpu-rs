7885f687c7bed66a0fde2f6fc53a85856aa50016
Flakebi <flakebi@t-online.de>
2026-05-26 08:46:51 +0200

    Expose build flags

--- amdgpu-device-libs-build/src/lib.rs
+++ amdgpu-device-libs-build/src/lib.rs
@@ -22,6 +22,56 @@ use std::collections::HashSet;
 
 use rustflags::Flag;
 
+#[derive(Default)]
+pub struct Build {
+    pub link_args: Vec<String>,
+    pub used_env_vars: Vec<String>,
+    pub used_files: Vec<String>,
+}
+
+pub fn get_link_args(mut is_wave64_enabled: bool, target_cpu: &str) -> Build {
+    let mut build = Build::default();
+
+    let cur_dir = env!("CARGO_MANIFEST_DIR");
+
+    let rocm_path = std::env::var("ROCM_PATH").unwrap();
+    let rocm_device_lib_path = std::env::var("ROCM_DEVICE_LIB_PATH").unwrap_or(rocm_path);
+    let device_libs = format!("{}/amdgcn/bitcode", rocm_device_lib_path);
+    build.used_env_vars.push("ROCM_PATH".into());
+    build.used_env_vars.push("ROCM_DEVICE_LIB_PATH".into());
+
+    let gfxip = target_cpu
+        .strip_prefix("gfx")
+        .unwrap_or_else(|| panic!("target-cpu '{target_cpu}' did not start with gfx"));
+
+    build.link_args.push(format!("{device_libs}/ockl.bc"));
+    build
+        .link_args
+        .push(format!("{device_libs}/oclc_isa_version_{gfxip}.bc"));
+    build
+        .link_args
+        .push(format!("{device_libs}/oclc_abi_version_600.bc"));
+
+    // wave64 is the default on gfx9 and before
+    is_wave64_enabled |= gfxip.starts_with('9') && gfxip.len() == 3;
+    is_wave64_enabled |= gfxip.starts_with("9-") && gfxip.ends_with("-generic");
+    let wavesize = if is_wave64_enabled { 64 } else { 32 };
+    build.link_args.push(format!(
+        "{device_libs}/oclc_wavefrontsize64_{}.bc",
+        if is_wave64_enabled { "on" } else { "off" }
+    ));
+
+    build
+        .used_files
+        .push(format!("{cur_dir}/util{wavesize}.bc"));
+    build.link_args.push(format!("{cur_dir}/util{wavesize}.bc"));
+
+    // Workarounds to make linker-plugin-lto work
+    build.link_args.push("--undefined-version".into());
+    build.link_args.push("--no-gc-sections".into());
+    build
+}
+
 /// Link libraries for `amdgpu-device-libs`.
 ///
 /// Call in a cargo buildscript:
@@ -32,15 +82,6 @@ use rustflags::Flag;
 /// }
 /// ```
 pub fn build() {
-    let cur_dir = env!("CARGO_MANIFEST_DIR");
-    println!("cargo::rerun-if-env-changed=CARGO_CFG_TARGET_FEATURE");
-
-    let rocm_path = std::env::var("ROCM_PATH").unwrap();
-    let rocm_device_lib_path = std::env::var("ROCM_DEVICE_LIB_PATH").unwrap_or(rocm_path);
-    let device_libs = format!("{}/amdgcn/bitcode", rocm_device_lib_path);
-    println!("cargo::rerun-if-env-changed=ROCM_PATH");
-    println!("cargo::rerun-if-env-changed=ROCM_DEVICE_LIB_PATH");
-
     // Find out target cpu and enabled features
     let mut target_features = std::env::var("CARGO_CFG_TARGET_FEATURE")
         .unwrap_or_default()
@@ -67,36 +108,17 @@ pub fn build() {
         }
     }
     let target_cpu = target_cpu.expect("Did not find target-cpu in RUSTFLAGS");
-    let gfxip = target_cpu
-        .strip_prefix("gfx")
-        .unwrap_or_else(|| panic!("target-cpu '{target_cpu}' did not start with gfx"));
+    let is_wave64_enabled = target_features.contains("wavefrontsize64");
+    let mut build = get_link_args(is_wave64_enabled, &target_cpu);
+    build.used_env_vars.push("CARGO_CFG_TARGET_FEATURE".into());
 
-    println!("cargo::rustc-link-arg={}/ockl.bc", device_libs);
-    println!(
-        "cargo::rustc-link-arg={}/oclc_isa_version_{}.bc",
-        device_libs, gfxip,
-    );
-    println!(
-        "cargo::rustc-link-arg={}/oclc_abi_version_600.bc",
-        device_libs,
-    );
-
-    let mut is_wave64 = target_features.contains("wavefrontsize64");
-    // wave64 is the default on gfx9 and before
-    is_wave64 |= gfxip.starts_with('9') && gfxip.len() == 3;
-    is_wave64 |= gfxip.starts_with("9-") && gfxip.ends_with("-generic");
-    let is_wave64 = is_wave64;
-    let wavesize = if is_wave64 { 64 } else { 32 };
-    println!(
-        "cargo::rustc-link-arg={}/oclc_wavefrontsize64_{}.bc",
-        device_libs,
-        if is_wave64 { "on" } else { "off" },
-    );
-
-    println!("cargo::rerun-if-changed={}/util{}.bc", cur_dir, wavesize);
-    println!("cargo::rustc-link-arg={}/util{}.bc", cur_dir, wavesize);
-
-    // Workarounds to make linker-plugin-lto work
-    println!("cargo::rustc-link-arg=--undefined-version");
-    println!("cargo::rustc-link-arg=--no-gc-sections");
+    for v in &build.used_env_vars {
+        println!("cargo::rerun-if-env-changed={v}");
+    }
+    for f in &build.used_files {
+        println!("cargo::rerun-if-changed={f}");
+    }
+    for a in &build.link_args {
+        println!("cargo::rustc-link-arg={a}");
+    }
 }
